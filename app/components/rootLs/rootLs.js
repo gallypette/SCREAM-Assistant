43f53dde68c235122b4e4220d724fd334e8a106e
gally <huynenjl@gmail.com>
2015-07-07 15:44:45 +0200

    Initial implementation of #3

--- app/components/rootLs/rootLs.js
+++ app/components/rootLs/rootLs.js
@@ -1,38 +1,60 @@
 'use strict';
-
 angular.module('myApp.rootLs', [])
 
-        // Repository store handler
-        .factory('Root', function (_, localStorageService) {
+	// Repository store handler
+	.factory('Root', function (_, localStorageService, $filter) {
 
-            return {
-                "getAnalyses": function () {
-                    var anal = localStorageService.get('repository') || [];
-                    return anal;
-                },
-                "getAnalysis": function (match) {
-                    var anal = localStorageService.get('repository') || [];
-                    return anal = $filter('filter')({name: match}, true);
-                },
-                "addAnalysis": function (newAnalysis) {
-                    var anal = localStorageService.get('repository') || [];
-                    anal.push(newAnalysis);
-                    console.log(anal);
-                    localStorageService.set('repository', anal);
-                },
-                "deleteAnalysis": function (delAnalysis) {
-                    var anal = localStorageService.get('repository') || [];
-                    anal = _.reject(anal, function (item) {
-                        return (item.name == delAnalysis.name)
-                    });
-                    localStorageService.set('repository', anal);
-                }
-            }
-        })
+		return {
+			"getAnalyses": function () {
+				var anal = localStorageService.get('repository') || [];
+				return anal;
+			},
+			"getAnalysis": function (name) {
+				var anal = localStorageService.get('repository') || [];
+				return $filter('filter')(anal, {name: name}, true);
+			},
+			"addAnalysis": function (newAnalysis) {
+				var anal = localStorageService.get('repository') || [];
+				anal.push(newAnalysis);
+				localStorageService.set('repository', anal);
+			},
+			"deleteAnalysis": function (delAnalysis) {
+				var anal = localStorageService.get('repository') || [];
+				anal = _.reject(anal, function (item) {
+					return (item.name == delAnalysis.name)
+				});
+				localStorageService.set('repository', anal);
+			}
+		}
+	})
 
-        // Current analysis store handler
-        .factory('Curr', function (_, localStorageService) {
+	// Current analysis store handler
+	.factory('Current', function (_, localStorageService, Root) {
 
+		// Private methods
+		// Take an analysis and set it as the analysis under scrutiny
+		var setCurrent = function (analysis) {
+			localStorageService.set('current', analysis);
+		}
 
-        })
-;
+		// Save the current Analysis into the repository
+		var saveCurrent = function () {
+			var curr = localStorageService.get('current') || [];
+			// First, we call Root.getAnalysis to see if we need to update or create
+			if (_.isUndefined(Root.getAnalysis(curr.name))) {
+				addAnalysis(curr);
+			} else {// Update
+				console.log('update !');
+				Root.deleteAnalysis(curr);
+				Root.addAnalysis(curr)
+			}
+		}
+
+		return{
+			"selectCurrent": function (analysis) {
+				saveCurrent();
+				setCurrent(analysis);
+			}
+		}
+	})
+	;
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,44 +2,49 @@
 
 angular.module('myApp.view1', ['ngRoute', 'myApp.rootLs', 'underscore', 'ngReally'])
 
-        .config(['$routeProvider', function ($routeProvider) {
-                $routeProvider.when('/view1', {
-                    templateUrl: 'view1/view1.html',
-                    controller: 'View1Ctrl'
-                });
-            }])
+	.config(['$routeProvider', function ($routeProvider) {
+			$routeProvider.when('/view1', {
+				templateUrl: 'view1/view1.html',
+				controller: 'View1Ctrl'
+			});
+		}])
 
-        .controller('View1Ctrl', function ($scope, Root, _) {
+	.controller('View1Ctrl', function ($scope, Root, Current, _) {
 
-            $scope.analyses = Root.getAnalyses();
-            $scope.unique = true;
+		$scope.analyses = Root.getAnalyses();
+		$scope.unique = true;
 
-            $scope.$watch('analyses', function (newValue, oldValue) {
-                // Add
-                if (newValue.length > oldValue.length) {
-                    (_.isEmpty($scope.analysis)) ? "" : Root.addAnalysis($scope.analysis);
-                    $scope.analysis = ''
-                }
-            }, true);
+		$scope.$watch('analyses', function (newValue, oldValue) {
+			// Add
+			if (newValue.length > oldValue.length) {
+				(_.isEmpty($scope.analysis)) ? "" : Root.addAnalysis($scope.analysis);
+				$scope.analysis = ''
+			}
+		}, true);
 
-            $scope.addAnalysis = function () {
-                if (_.isUndefined(_.find($scope.analyses, function (item) {
-                    return ($scope.analysis.name == item.name)
-                }))) {
-                    $scope.unique = true;
-                    $scope.analysis.date = new Date();
-                    $scope.analyses.push($scope.analysis);
-                } else {
-                    $scope.unique = false;
-                    console.log($scope.unique);
-                }
-            }
+		$scope.addAnalysis = function () {
+			if (_.isUndefined(_.find($scope.analyses, function (item) {
+				return ($scope.analysis.name == item.name)
+			}))) {
+				$scope.unique = true;
+				$scope.analysis.date = new Date();
+				$scope.analyses.push($scope.analysis);
+			} else {
+				$scope.unique = false;
+				console.log($scope.unique);
+			}
+		}
 
-            $scope.deleteAnalysis = function (analysis) {
+		$scope.deleteAnalysis = function (analysis) {
 //                console.log("deleting "+analysis.name);
-                $scope.analyses = _.reject($scope.analyses, function (item) {
-                    return ((item.name == analysis.name) && (item.desc == analysis.desc))
-                });
-                Root.deleteAnalysis(analysis);
-            }
-        });
+			$scope.analyses = _.reject($scope.analyses, function (item) {
+				return ((item.name == analysis.name) && (item.desc == analysis.desc))
+			});
+			Root.deleteAnalysis(analysis);
+		}
+
+		$scope.selectAnalysis = function (analysis) {
+//                console.log("selecting "+analysis.name);
+			Current.selectCurrent(analysis);
+		}
+	});
