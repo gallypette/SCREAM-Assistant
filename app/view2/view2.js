f72a30ac5bd4809643a01182002aae59ebfbbeac
gallypette <huynenjl@gmail.com>
2015-07-12 07:06:45 +0200

    wires view2 for xsltProcessor dev

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -2,21 +2,31 @@
 
 angular.module('myApp.view2', [])
 
-        .config(['$routeProvider', function ($routeProvider) {
-                $routeProvider.when('/view2', {
-                    templateUrl: 'view2/view2.html',
-                    controller: 'View2Ctrl'
-                });
-            }])
+	.config(['$routeProvider', function ($routeProvider) {
+			$routeProvider.when('/view2', {
+				templateUrl: 'view2/view2.html',
+				controller: 'View2Ctrl'
+			});
+		}])
 
-        .controller('View2Ctrl', function ($scope, schemasFactory) {
+	.controller('View2Ctrl', function ($scope, schemasFactory, xsltTransform) {
 
-            $scope.creamtab = {};
-            schemasFactory.getFile('creamTables.json')
-                    .then(function (response) {
-                        $scope.creamtab = response;
-                    }, function (error) {
-                        console.error(error);
-                    });
+		var getScheme = function (scheme) {
+			schemasFactory.getFile(scheme)
+				.then(function (response) {
+					return response;
+				}, function (error) {
+					console.error(error);
+					return(error);
+				});
+		}
 
-        });
+		var transform = function (xml, xsl) {
+			xml = getScheme('cdcatalog.xml');
+			xsl = getScheme('cdcatalog.xsl');
+			return xsltTransform.trans(xml, xsl);
+		}
+
+		$scope.result = transform('cdcatalog.xml', 'cdcatalog.xsl');
+
+	});
