9eb41210e5532d5c2fb85c6dffd484ba540cb0ca
gally <huynenjl@gmail.com>
2015-09-01 15:34:12 +0200

    wip: implementing ams : beginning

--- app/view5/view5.js
+++ app/view5/view5.js
@@ -3,13 +3,24 @@
 angular.module('myApp.view5', [])
 
 	.config(['$routeProvider', function ($routeProvider) {
-			$routeProvider.when('/view5', {
+			$routeProvider.when('/view5/:id', {
 				templateUrl: 'view5/view5.html',
-				controller: 'View5Ctrl'
+				controller: 'View5Ctrl',
+				resolve: {
+					stc: function ($route, Stc, Am) {
+						return Stc.find($route.current.params.id).then(function (stc) {
+//							Am.create({stcId: stc.id});
+							
+//							console.log(stc.DSLoadRelations(['am']))
+							return Stc.loadRelations(stc.id, ['am']);
+						});
+					}
+				}
 			});
+			// .when we land on the view5's root, get the current Stc
 		}])
 
-	.controller('View5Ctrl', function ($scope, analysisMenu) {
+	.controller('View5Ctrl', function ($route, $scope, analysisMenu) {
 
 		$scope.itemsMenu = analysisMenu;
 		$scope.isActive = function (url) {
@@ -18,5 +29,11 @@ angular.module('myApp.view5', [])
 		$scope.isActiveM = function (url) {
 			return url === "#/viewAttackAnalysis" ? 'active' : 'brand';
 		}
+
+
+		// Load the data into the view
+
+		$scope.stc = $route.current.locals.stc;
+		console.log($scope.stc.ams);
 
 	});
