a81f61c30a0d5be8d8bd5a6f1e1fad6d2b464a9b
gally <huynenjl@gmail.com>
2015-09-01 16:27:27 +0200

    wip: implementing ams : playing with promises

--- app/view5/view5.js
+++ app/view5/view5.js
@@ -3,21 +3,32 @@
 angular.module('myApp.view5', [])
 
 	.config(['$routeProvider', function ($routeProvider) {
-			$routeProvider.when('/view5/:id', {
-				templateUrl: 'view5/view5.html',
-				controller: 'View5Ctrl',
-				resolve: {
-					stc: function ($route, Stc, Am) {
-						return Stc.find($route.current.params.id).then(function (stc) {
+			$routeProvider.
+				// when we land directly on the stc by the use of the button
+				when('/view5/:id', {
+					templateUrl: 'view5/view5.html',
+					controller: 'View5Ctrl',
+					resolve: {
+						stc: function ($route, Stc, Am) {
+							return Stc.find($route.current.params.id).then(function (stc) {
 //							Am.create({stcId: stc.id});
-							
-//							console.log(stc.DSLoadRelations(['am']))
-							return Stc.loadRelations(stc.id, ['am']);
-						});
+								return Stc.loadRelations(stc.id, ['am']);
+							});
+						}
 					}
-				}
-			});
-			// .when we land on the view5's root, get the current Stc
+				}).
+				// .when we land on the view5's root, we need to get the current stc
+				when('/view5', {
+					templateUrl: 'view5/view5.html',
+					controller: 'View5Ctrl',
+					resolve: {
+						stc: function ($route, Stc, Am) {
+							return Stc.findAll({current: 'true'}).then(function (stc) {
+								return Stc.loadRelations(stc[0].id, ['am']);
+							});
+						}
+					}
+				});
 		}])
 
 	.controller('View5Ctrl', function ($route, $scope, analysisMenu) {
@@ -34,6 +45,7 @@ angular.module('myApp.view5', [])
 		// Load the data into the view
 
 		$scope.stc = $route.current.locals.stc;
+		console.log($scope.stc);
 		console.log($scope.stc.ams);
 
 	});
