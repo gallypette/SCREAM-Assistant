bbb134b5f28d63b8ef14923dc6b18fd15ca6023c
gally <huynenjl@gmail.com>
2015-09-07 12:34:13 +0200

    wip: analysis : initial commits

--- app/view3/view3.js
+++ app/view3/view3.js
@@ -3,13 +3,44 @@
 angular.module('myApp.view3', [])
 
 	.config(['$routeProvider', function ($routeProvider) {
-			$routeProvider.when('/view3', {
-				templateUrl: 'view3/view3.html',
-				controller: 'View3Ctrl'
-			});
+			$routeProvider.
+				// when we land directly on the analysis by the use of the main menu's button
+				when('/view3/:id', {
+					templateUrl: 'view3/view3.html',
+					controller: 'View3Ctrl',
+					resolve: {
+						atck: function ($route, Stc, Atck, Analysis, Description) {
+							return Atck.find($route.current.params.id).then(function (atck) {
+								return Atck.loadRelations(atck.id, ['analysis', 'description']);
+							});
+						},
+						current: function ($route, Stc, Atck, Analysis, Description) {
+							return Atck.findAll({current: 'true'});
+						}
+					}
+				}).
+				// .when we land on the view3's root, we need to get the current analysis
+				when('/view3', {
+					templateUrl: 'view3/view3.html',
+					controller: 'View3Ctrl',
+					resolve: {
+						atck: function ($route, Stc, Atck, Analysis, Description) {
+							return Atck.findAll({current: 'true'}).then(function (atcks) {
+								return Atck.loadRelations(atcks[0].id, ['analysis', 'description']);
+							});
+						},
+						current: function ($route, Stc, Atck, Analysis, Description) {
+							return Atck.findAll({current: 'true'});
+						}
+					}
+				});
 		}])
 
-	.controller('View3Ctrl', function ($scope, analysisMenu) {
+	.controller('View3Ctrl', function ($scope, $route, analysisMenu, Analysis, Atck, Description) {
+		
+		console.log($route.current.locals.atck);
+		console.log($route.current.locals.atck.description);
+		console.log($route.current.locals.atck.analysis);
 
 		$scope.itemsMenu = analysisMenu;
 
