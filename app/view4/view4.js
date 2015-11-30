3467e0eec890a7edc953ea85b1f21bff67f2fb7c
gally <huynenjl@gmail.com>
2015-11-30 15:08:02 +0100

    Scaffold of the results view

--- app/view4/view4.js
+++ app/view4/view4.js
@@ -3,14 +3,50 @@
 angular.module('myApp.view4', [])
 
 	.config(['$routeProvider', function ($routeProvider) {
-			$routeProvider.when('/view4', {
-				templateUrl: 'view4/view4.html',
-				controller: 'View4Ctrl'
-			});
+			$routeProvider.
+				// when we land directly on the analysis by the use of the main menu's button
+				when('/view4/:id', {
+					templateUrl: 'view4/view4.html',
+					controller: 'View4Ctrl',
+					resolve: {
+						atck: function ($route, Stc, Atck, Analysis, Description) {
+							return Atck.find($route.current.params.id).then(function (atck) {
+								return Atck.loadRelations(atck.id, ['analysis', 'description']);
+							});
+						},
+						current: function ($route, Stc, Atck, Analysis, Description, ErrorMode) {
+							return Atck.find($route.current.params.id).then(function (atck) {
+								return Atck.loadRelations(atck.id, ['analysis']).then(function (atck) {
+									return ErrorMode.findAll({current: 'true', analysisId: atck.analysis.id}, {cacheResponse: false});
+								});
+							});
+						}
+					}
+				}).
+				// .when we land on the view4's root, we need to get the current analysis
+				when('/view4', {
+					templateUrl: 'view4/view4.html',
+					controller: 'View4Ctrl',
+					resolve: {
+						atck: function ($route, Stc, Atck, Analysis, Description) {
+							return Atck.findAll({current: 'true'}).then(function (atcks) {
+								return Atck.loadRelations(atcks[0].id, ['analysis', 'description']);
+							});
+						},
+						current: function ($route, Stc, Atck, Analysis, Description, ErrorMode) {
+							return Atck.findAll({current: 'true'}).then(function (atck) {
+								return Atck.loadRelations(atck[0].id, ['analysis']).then(function (atck) {
+									return ErrorMode.findAll({current: 'true', analysisId: atck.analysis.id}, {cacheResponse: false});
+								});
+							});
+						}
+					}
+				});
 		}])
 
-	.controller('View4Ctrl', function ($scope, analysisMenu) {
+	.controller('View4Ctrl', function ($scope, $route, $q, analysisMenu, Analysis, Atck, ErrorMode) {
 
+		// Menu vars from app constant
 		$scope.itemsMenu = analysisMenu;
 		$scope.isActive = function (url) {
 			return url === "#/view4" ? 'active' : '';
@@ -19,4 +55,24 @@ angular.module('myApp.view4', [])
 			return url === "#/viewAttackAnalysis" ? 'active' : 'brand';
 		}
 
+		// View var from resolve 
+		$scope.atck = $route.current.locals.atck;
+		$scope.current = $route.current.locals.current[0];
+		
+		// lazy loading of nested realtions does not work with localstorage
+		// so we resolve those here
+		$q.resolve(Analysis.loadRelations($scope.atck.analysis.id)).then(function () {
+			// The model where we store the values linked in the view (form)
+			// We need to populated it with the analysis's error modes.
+			$scope.model = {};
+			if (_.isEmpty($scope.atck.analysis.ems)) {
+				return true;
+			} else {
+				_.each($scope.atck.analysis.ems, function (value, key, list) {
+					$scope.model[value.category] = value.em;
+				});
+				return true;
+			}
+		});
+	
 	});
