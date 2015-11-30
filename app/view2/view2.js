36032ae53dd163c8cfa756ba186f7232353d2840
gally <huynenjl@gmail.com>
2015-11-30 16:14:10 +0100

    dummy scaffolds of view2

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -1,16 +1,54 @@
 'use strict';
 
+// I put this here as a scaffold but the real view needs to bring almost all the app's data model.
+
 angular.module('myApp.view2', [])
 
 	.config(['$routeProvider', function ($routeProvider) {
-			$routeProvider.when('/view2', {
-				templateUrl: 'view2/view2.html',
-				controller: 'View2Ctrl'
-			});
+			$routeProvider.
+				// when we land directly on the analysis by the use of the main menu's button
+				when('/view2/:id', {
+					templateUrl: 'view2/view2.html',
+					controller: 'View2Ctrl',
+					resolve: {
+						atck: function ($route, Stc, Atck, Analysis, Description) {
+							return Atck.find($route.current.params.id).then(function (atck) {
+								return Atck.loadRelations(atck.id, ['analysis', 'description']);
+							});
+						},
+						current: function ($route, Stc, Atck, Analysis, Description, ErrorMode) {
+							return Atck.find($route.current.params.id).then(function (atck) {
+								return Atck.loadRelations(atck.id, ['analysis']).then(function (atck) {
+									return ErrorMode.findAll({current: 'true', analysisId: atck.analysis.id}, {cacheResponse: false});
+								});
+							});
+						}
+					}
+				}).
+				// .when we land on the view4's root, we need to get the current analysis
+				when('/view2', {
+					templateUrl: 'view2/view2.html',
+					controller: 'View2Ctrl',
+					resolve: {
+						atck: function ($route, Stc, Atck, Analysis, Description) {
+							return Atck.findAll({current: 'true'}).then(function (atcks) {
+								return Atck.loadRelations(atcks[0].id, ['analysis', 'description']);
+							});
+						},
+						current: function ($route, Stc, Atck, Analysis, Description, ErrorMode) {
+							return Atck.findAll({current: 'true'}).then(function (atck) {
+								return Atck.loadRelations(atck[0].id, ['analysis']).then(function (atck) {
+									return ErrorMode.findAll({current: 'true', analysisId: atck.analysis.id}, {cacheResponse: false});
+								});
+							});
+						}
+					}
+				});
 		}])
 
-	.controller('View2Ctrl', function ($sce, $scope, $q, schemasFactory, xsltTransform, analysisMenu) {
+	.controller('View2Ctrl', function ($scope, $route, $q, analysisMenu, Analysis, Atck, ErrorMode) {
 
+		// Menu vars from app constant
 		$scope.itemsMenu = analysisMenu;
 		$scope.isActive = function (url) {
 			return url === "#/view2" ? 'active' : '';
@@ -19,40 +57,24 @@ angular.module('myApp.view2', [])
 			return url === "#/viewAttackAnalysis" ? 'active' : 'brand';
 		}
 
-		var parameters = {
-			"param1": "Mon premier paramètre",
-			"param2": "Mon deuxième paramètre",
-			"param3": "Mon troisième paramètre"
-		};
-
-		var getScheme = function (scheme) {
-		}
-
-		var transform = function (xml, xsl) {
-
-			var xmlFile = schemasFactory.getFile(xml)
-				.then(function (response) {
-					return response;
-				}, function (error) {
-					console.error(error);
-					return(error);
-				});
-
-			var xslFile = schemasFactory.getFile(xsl)
-				.then(function (response) {
-					return response;
-				}, function (error) {
-					console.error(error);
-					return(error);
-				});
-
-			$q.all([xmlFile, xslFile]).then(function wrapUp(files) {
-				var transformResult = xsltTransform.transformXml(files[0].data, files[1].data, parameters);
-				console.log(transformResult);
-				$scope.result = $sce.trustAsHtml(transformResult);
+		// View var from resolve 
+		$scope.atck = $route.current.locals.atck;
+		$scope.current = $route.current.locals.current[0];
+		
+		// lazy loading of nested realtions does not work with localstorage
+		// so we resolve those here
+		$q.resolve(Analysis.loadRelations($scope.atck.analysis.id)).then(function () {
+			// The model where we store the values linked in the view (form)
+			// We need to populated it with the analysis's error modes.
+			$scope.model = {};
+			if (_.isEmpty($scope.atck.analysis.ems)) {
 				return true;
-			});
-		}
-
-		transform('cdcatalog.xml', 'cdcatalog.xsl', parameters);
+			} else {
+				_.each($scope.atck.analysis.ems, function (value, key, list) {
+					$scope.model[value.category] = value.em;
+				});
+				return true;
+			}
+		});
+	
 	});
