fbd8cb304740d9875e1cbc8b08ebcae028f920b4
gally <huynenjl@gmail.com>
2016-01-14 07:15:50 +0100

    Check if the system is described before displaying any results

--- app/viewSystemResults/viewSystemResults.js
+++ app/viewSystemResults/viewSystemResults.js
@@ -37,7 +37,7 @@ angular.module('myApp.viewSystemResults', [])
 				});
 		}])
 
-	.controller('ViewSystemResultsCtrl', function ($scope, $route, $q, screamMenu, Analysis, Atck, ErrorMode, Stc, errorModes, threatModel) {
+	.controller('ViewSystemResultsCtrl', function ($modal, $location, $scope, $route, $q, screamMenu, Analysis, Atck, ErrorMode, Stc, errorModes, threatModel) {
 
 		$scope.areCompleted = function () {
 			_.each($scope.atck.analysis.ems, function (value, key, list) {
@@ -53,105 +53,122 @@ angular.module('myApp.viewSystemResults', [])
 		// View var from resolve 
 		$scope.sys = $route.current.locals.sys;
 
-		// Analizing the attacks linked to this system
-		var promises = [];
-		$scope.antecedents = [];
-		_.each($scope.sys.atcks, function (element, index, list) {
-			promises.push(Atck.loadRelations(element.id).then(function () {
-				return Analysis.loadRelations($scope.sys.atcks[index].analysis.id).then(function () {
-					if (_.isEmpty($scope.sys.atcks[index].analysis.ems)) {
-						console.log('No error modes');
-						return true;
-					} else {
-						_.each($scope.sys.atcks[index].analysis.ems, function (value, key, list) {
-							// For each ErrorMode, we check that it reached an end state
-							value.completed = errorModes.analysisCompleted(value);
-							// For each ErrorMode, we compile the list of antecedents
-							if (value.completed) {
-								$scope.sys.atcks[index].analysis.ems[key].antecedents = [];
-								$scope.sys.atcks[index].analysis.ems[key].antecedents = (errorModes.analysisResults(value));
-							}
-						});
-					}
-					return true;
-				})
-			}));
-		});
-		$q.all(promises).then(function (values) {
-			console.log('Linked Attacks analyzed');
-		});
-
-		// Compiling the STCs corresponding to this system
-		var promisesStc = [];
-		// Get compatible-linked to a STC-atcks
-		threatModel.speStc($scope.sys).then(function (atcksCompatibles) {
-			// get the corresponding STC, analyses, ems, and antecedents.
-			var deferred = [];
-			_.each(atcksCompatibles, function (atck) {
-				deferred.push(Atck.loadRelations(atck.id, ['stc']));
+		// First we check if this system has a description
+		if (_.isUndefined($scope.sys.description)) {
+			var modalInstance = $modal.open({
+				animation: true,
+				templateUrl: 'warningModalNoDesc',
+				size: 'sg',
+				controller: function ($scope, $modalInstance, $location) {
+					// Dimiss the modal when OK is clicked.
+					$scope.cancel = function () {
+						$modalInstance.dismiss('cancel');
+						$location.path('/viewSystems/');
+					};
+				}
 			});
-			var stcs = [];
-			$q.all(deferred).then(function (values) {
-				// We build the stcs array
-				_.each(values, function (element) {
-					stcs.push(element.stc);
-				});
-				stcs = _.uniq(stcs);
-				return stcs;
-			}).then(function (stcs) {
-				var deferred = [];
-				_.each(stcs, function (element) {
-					// We load the relations of the Stc 
-					deferred.push(Stc.loadRelations(element.id, ['atck']));
-				});
-				$q.all(deferred).then(function (values) {
-					// Now that the stc are populated by the attacks, we push
-					// only the compatible into the scope
-					_.each(values, function (element, index, list) {
-						stcs[index].atcks = _.intersection(element.atcks, atcksCompatibles);
-					});
-					return stcs;
-					// The list of Stc is now ready
-				}).then(function (stcs) {
-					$scope.stcs = stcs;
-					var promises = [];
-					_.each($scope.stcs, function (elementStc, indexStc, listStc) {
-						elementStc.antecedents = [];
-						_.each(elementStc.atcks, function (elementAtck, indexAtck, listAtck) {
-							promises.push(Atck.loadRelations(elementAtck.id, ['description', 'analysis']).then(function () {
-								return Analysis.loadRelations(elementAtck.analysis.id).then(function () {
-									if (_.isEmpty(elementAtck.analysis.ems)) {
-										console.log('No error modes');
-										return true;
-									} else {
-										_.each(elementAtck.analysis.ems, function (value, key, list) {
-											// For each ErrorMode, we check that it reached an end state
-											value.completed = errorModes.analysisCompleted(value);
+		} else {
 
-											// For each ErrorMode, we compile the list of antecedents
-											if (value.completed) {
-												$scope.stcs[indexStc].antecedents.push({ant: errorModes.analysisResults(value), em: value, description: stcs[indexStc].atcks[indexAtck].description});
-											}
-										});
-									}
-									return true;
-								})
-							}));
-						});
-						$q.all(promises).then(function () {
-							$scope.stcs[indexStc].antecedents = errorModes.analysisResultsSTC($scope.stcs[indexStc].antecedents);
-							var keys = [];
-							_.each($scope.stcs[indexStc].specifics, function (val, key) {
-								if (val) {
-									keys.push(key);
+			// Analizing the attacks linked to this system
+			var promises = [];
+			$scope.antecedents = [];
+			_.each($scope.sys.atcks, function (element, index, list) {
+				promises.push(Atck.loadRelations(element.id).then(function () {
+					return Analysis.loadRelations($scope.sys.atcks[index].analysis.id).then(function () {
+						if (_.isEmpty($scope.sys.atcks[index].analysis.ems)) {
+							console.log('No error modes');
+							return true;
+						} else {
+							_.each($scope.sys.atcks[index].analysis.ems, function (value, key, list) {
+								// For each ErrorMode, we check that it reached an end state
+								value.completed = errorModes.analysisCompleted(value);
+								// For each ErrorMode, we compile the list of antecedents
+								if (value.completed) {
+									$scope.sys.atcks[index].analysis.ems[key].antecedents = [];
+									$scope.sys.atcks[index].analysis.ems[key].antecedents = (errorModes.analysisResults(value));
 								}
 							});
-							$scope.stcs[indexStc].specifics = keys;
+						}
+						return true;
+					})
+				}));
+			});
+			$q.all(promises).then(function (values) {
+				console.log('Linked Attacks analyzed');
+			});
+
+			// Compiling the STCs corresponding to this system
+			var promisesStc = [];
+			// Get compatible-linked to a STC-atcks
+			threatModel.speStc($scope.sys).then(function (atcksCompatibles) {
+				// get the corresponding STC, analyses, ems, and antecedents.
+				var deferred = [];
+				_.each(atcksCompatibles, function (atck) {
+					deferred.push(Atck.loadRelations(atck.id, ['stc']));
+				});
+				var stcs = [];
+				$q.all(deferred).then(function (values) {
+					// We build the stcs array
+					_.each(values, function (element) {
+						stcs.push(element.stc);
+					});
+					stcs = _.uniq(stcs);
+					return stcs;
+				}).then(function (stcs) {
+					var deferred = [];
+					_.each(stcs, function (element) {
+						// We load the relations of the Stc 
+						deferred.push(Stc.loadRelations(element.id, ['atck']));
+					});
+					$q.all(deferred).then(function (values) {
+						// Now that the stc are populated by the attacks, we push
+						// only the compatible into the scope
+						_.each(values, function (element, index, list) {
+							stcs[index].atcks = _.intersection(element.atcks, atcksCompatibles);
+						});
+						return stcs;
+						// The list of Stc is now ready
+					}).then(function (stcs) {
+						$scope.stcs = stcs;
+						var promises = [];
+						_.each($scope.stcs, function (elementStc, indexStc, listStc) {
+							elementStc.antecedents = [];
+							_.each(elementStc.atcks, function (elementAtck, indexAtck, listAtck) {
+								promises.push(Atck.loadRelations(elementAtck.id, ['description', 'analysis']).then(function () {
+									return Analysis.loadRelations(elementAtck.analysis.id).then(function () {
+										if (_.isEmpty(elementAtck.analysis.ems)) {
+											console.log('No error modes');
+											return true;
+										} else {
+											_.each(elementAtck.analysis.ems, function (value, key, list) {
+												// For each ErrorMode, we check that it reached an end state
+												value.completed = errorModes.analysisCompleted(value);
+
+												// For each ErrorMode, we compile the list of antecedents
+												if (value.completed) {
+													$scope.stcs[indexStc].antecedents.push({ant: errorModes.analysisResults(value), em: value, description: stcs[indexStc].atcks[indexAtck].description});
+												}
+											});
+										}
+										return true;
+									})
+								}));
+							});
+							$q.all(promises).then(function () {
+								$scope.stcs[indexStc].antecedents = errorModes.analysisResultsSTC($scope.stcs[indexStc].antecedents);
+								var keys = [];
+								_.each($scope.stcs[indexStc].specifics, function (val, key) {
+									if (val) {
+										keys.push(key);
+									}
+								});
+								$scope.stcs[indexStc].specifics = keys;
+							});
 						});
 					});
 				});
 			});
-		});
+		}
 
 		// Filter the antecedents defined as specific by the STC
 		$scope.filterSpecifics = function (stc) {
